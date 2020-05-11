312f88ac1e0ad7b14313cef145f6d3d36719a378
Marvin Eversley Silva <meverss@outlook.com>
2020-05-11 12:52:51 -0400

    New stuff

--- Excercises/sudoku.py
+++ Excercises/sudoku.py
@@ -4,49 +4,49 @@ from random import shuffle
 clear = system("clear")
 clear
 
-row = []
 sudoku = []
-box = []
+row  = ''
+
+#rng = []
+#for i in range(1,10):
+#  rng.append(i)
+
 i = 0
 while i <= 8:
-  rng = [1,2,3,4,5,6,7,8,9]
-  row.append(f"{rng[0:3]},{rng[3:6]},{rng[6:]}")
+  rng = str(123456789)
+#  row = (rng[0:3] +  rng[3:6] +  rng[6:])#.split(" ")
+  row = (f"{rng[0:3]} {rng[3:6]} {rng[6:]}").split(" ")
+
+  r = 0
+  while r <= 2:
+    row[r] = list(row[r])
+    r += 1
+  shuffle(row)
   sudoku.append(row)
-  row = []
+  print(int(row[0][0]) + int(row[0][1]) + int(row[0][2]))
   i += 1
 
-block0 = list(f"{sudoku[0][0][0]} {sudoku[0][0][1]} {sudoku[0][0][2]} \
+block0 = (f"{sudoku[0][0][0]} {sudoku[0][0][1]} {sudoku[0][0][2]} \
             {sudoku[1][0][0]} {sudoku[1][0][1]} {sudoku[1][0][2]} \
             {sudoku[2][0][0]} {sudoku[2][0][1]} {sudoku[2][0][2]}").split(" ")
 
-rng = []
-for i in range(1,10):
-  rng.append(i)
+#print(block0)
 
 def check1():
-#  bl1 = (f"{sudoku[0][0][0]} {sudoku[0][0][1]} {sudoku[0][0][2]} \
-#           {sudoku[1][0][0]} {sudoku[1][0][1]} {sudoku[1][0][2]} \
-#           {sudoku[2][0][0]} {sudoku[2][0][1]} {sudoku[2][0][2]}").split(" ")
-
-  ch = all(i in rng for i in block0)
-#  print(f"1 {bl1}\ch {ch}")
-  if ch == True:
-    print(f"Este: {ch}")
+  if any(x in rng for x in block0):
     return True
   else:
     return False
 
-
 print(check1())
 
 while check1() == True:
-#  sudoku[0][0].split(",")
-  shuffle(sudoku[0][0])
+  shuffle(sudoku[0])
 #  shuffle(sudoku[1])
 #  shuffle(sudoku[2])
 #  sudoku[0][0] + sudoku[0][1] + sudoku[0][2] + sudoku[1][0] + sudoku[1][1] + sudoku[1][2] + sudoku[2][0] + sudoku[2][1] + sudoku[2][2]
 #  check1()
-  print(sudoku[0][0])
+  print(sudoku[0])
 #### PRINT SUDOKU
 
 for a in sudoku:
