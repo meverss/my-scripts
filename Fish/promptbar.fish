7d60c3e6b560fb4ac44d2f060e6b189ddeeb0aad
Marvin Eversley Silva <meverss@gmail.com>
2021-05-28 22:58:48 -0400

    Updated scripts

--- Fish/promptbar.fish
+++ Fish/promptbar.fish
@@ -1,22 +1,33 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-#clear
-set text (string split '' ' Esto es una prueba de lo que hacemos... ')
+clear
+set text (string split '' ' Probando nuevo prompt... ')
 
-set n 189; set n1 $n
+for c1 in (seq 3)
+set n $c1"09"; set n1 $n
+#set n 109; set n1 $n
 set s (math -s0 (string length "$text")/20)
 if [ (string length "$text") -le 21 ]; set s 1; end
+
 for z in (seq 1 $s (count $text))
+echo $z - $s
     set t $text[$z..(math $z + $s - 1)]
     for x in (seq (count $t))
-	set y "$y"(string join0 (set_color -b $n1)$t[$x](set_color normal))
+	set y "$y"(set_color -b $n1)$t[$x](set_color -b normal)
     end
-    set txt (string join0 $y)
+
     if ! [ $n1 -eq (math $n - 9) ]
         set n1 (math $n1 - 1)
     end
+    set txt "* $y"
+
 end
+#end
 echo $txt
+end
+
+#end
+
 
 #for c in (seq 100 999)
 #    echo (set_color -b $c)" $c "(set_color normal) | more
--- Fish/test.fish
+++ Fish/test.fish
@@ -1,3 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/fish
+
+clear
+set format bold_ansi_code underline_ansi_code reversed_ansi_code reset_ansi_code teal_ansi_code green_ansi_code gold_ansi_code
 set -l bold_ansi_code "\u001b[1m"
 set -l underline_ansi_code "\u001b[4m"
 set -l reversed_ansi_code "\u001b[7m"
@@ -6,4 +10,6 @@ set -l teal_ansi_code "\u001b[38;5;31m"
 set -l green_ansi_code "\u001b[38;5;70m"
 set -l gold_ansi_code "\u001b[38;5;220m"
 
-echo "$bold_ansi_code Marvin"
+for x in (seq (count $$format))
+  printf "$$format[$x] Marvin"\n
+end
