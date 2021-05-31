af6c8df817ec3a941410cdccd5c561e1d5364933
Marvin Eversley Silva <meverss@gmail.com>
2021-05-31 05:43:59 -0400

    Added auto-install Powerline fonts

--- Fish/promptbar.fish
+++ Fish/promptbar.fish
@@ -1,34 +1,28 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-clear
-set text (string split '' ' Probando nuevo prompt... ')
+  clear
+  set text (string split '' ' Probando nuevo prompt... ')
 
-for c1 in (seq 3)
-set n $c1"09"; set n1 $n
-#set n 109; set n1 $n
-set s (math -s0 (string length "$text")/20)
-if [ (string length "$text") -le 21 ]; set s 1; end
+  set n 689; set n1 $n
+  set s (math -s0 (string length "$text")/20)
+  if [ (string length "$text") -le 21 ]; set s 1; end
 
-for z in (seq 1 $s (count $text))
-echo $z - $s
-    set t $text[$z..(math $z + $s - 1)]
-    for x in (seq (count $t))
-	set y "$y"(set_color -b $n1)$t[$x](set_color -b normal)
-    end
+  for z in (seq 1 $s (count $text))
+      set t $text[$z..(math $z + $s - 1)]
+      for x in (seq (count $t))
+  	set y "$y"(set_color -b $n1)$t[$x](set_color -b normal)
+      end
 
-    if ! [ $n1 -eq (math $n - 9) ]
+      if ! [ $n1 -eq (math $n - 9) ]
         set n1 (math $n1 - 1)
-    end
-    set txt "* $y"
+      end
+      set prompt $y
+  end
 
+  echo $prompt
+#--------------------------------
+for c in (seq 0 999)
+  if test $c -lt 10; set c "00"$c;end
+  if test $c -ge 10 -a $c -lt 100; set c "0"$c;end
+    echo (set_color -b $c)" $c "(set_color normal)
 end
-#end
-echo $txt
-end
-
-#end
-
-
-#for c in (seq 100 999)
-#    echo (set_color -b $c)" $c "(set_color normal) | more
-#end
--- Fish/test.fish
+++ Fish/test.fish
@@ -1,7 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
 clear
-read a
-#read -p 'echo Test: ' -n 10 a
-#read -p 'echo -n \n(set_color -b 777 6b052a)" "(set_color -b 777 000) $b_lang[7] (set_color -b normal 777)""(set_color normal)' -n 0 -l argv
-echo "Escribiste $a"
+read -p 'echo Escribe: ' -l argv
+if test $argv = 'marv'
+  echo Hacker
+else
+  echo Naah..
+end
+echo "Escribiste $argv" 
